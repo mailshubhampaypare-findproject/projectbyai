82abc3aab0e40e48b960d0eb17a531f47ef7a0df
Shubham Paypare <shubhampaypare@Shubhams-MacBook-Air.local>
2026-08-25 16:22:34 +0530

    feat: inject Google Analytics gtag.js tracking snippet in head of all pages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -106,6 +106,19 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NSX7X5GK50" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-NSX7X5GK50');
+            `,
+          }}
+        />
         <HeadContent />
       </head>
       <body>
